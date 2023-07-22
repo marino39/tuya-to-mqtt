a1a7b201d2da614dd33ff0d09247ba20f40b61f3
Marcin Gorzynski <marcin@gorzynski.me>
2023-07-22 18:03:36 +0200

    Hold current mqtt state to drop duplicate updates

--- application/tuya_to_mqtt_service.go
+++ application/tuya_to_mqtt_service.go
@@ -30,6 +30,9 @@ type TuyaToMQTTServiceParams struct {
 type tuyaToMQTTService struct {
 	params TuyaToMQTTServiceParams
 
+	mqttCurrentState      map[string]string
+	mqttCurrentStateMutex sync.RWMutex
+
 	log zerolog.Logger
 }
 
@@ -40,10 +43,10 @@ func NewTuyaToMQTTService(params TuyaToMQTTServiceParams) (TuyaToMQTTService, er
 	if params.MQTTClient == nil {
 		return nil, fmt.Errorf("MQTTClient is nil")
 	}
-	return &tuyaToMQTTService{params: params, log: params.Log}, nil
+	return &tuyaToMQTTService{params: params, mqttCurrentState: make(map[string]string), log: params.Log}, nil
 }
 
-func (t tuyaToMQTTService) Run(ctx context.Context) error {
+func (t *tuyaToMQTTService) Run(ctx context.Context) error {
 	g := errgroup.Group{}
 
 	rCtx, cancel := context.WithCancel(ctx)
@@ -51,6 +54,20 @@ func (t tuyaToMQTTService) Run(ctx context.Context) error {
 
 	initWaitGroup := sync.WaitGroup{}
 	initWaitGroup.Add(1)
+
+	// tuya load mqtt current state
+	g.Go(func() error {
+		err := t.params.MQTTClient.Subscribe(fmt.Sprintf("%s/#", t.params.MQTTTopic), 2, func(msg MQTTMessage) {
+			t.mqttCurrentStateMutex.Lock()
+			t.mqttCurrentState[msg.Topic()] = string(msg.Payload())
+			t.mqttCurrentStateMutex.Unlock()
+			msg.Ack()
+		})
+		if err != nil {
+			return err
+		}
+		return nil
+	})
 
 	// tuya message handler
 	g.Go(func() error {
@@ -128,11 +145,14 @@ func (t tuyaToMQTTService) Run(ctx context.Context) error {
 		}
 
 		for _, status := range statusList {
+			// build topic and value
 			topic := BuildMQTTTopicForStatusRaw(
 				t.params.MQTTTopic, "tuya", device.ProductID, device.ID, status.Code,
 			)
 			value := strings.ReplaceAll(fmt.Sprintf("%#v", status.Value), "\"", "")
-			err = t.params.MQTTClient.Publish(topic, 2, true, []byte(value))
+
+			// publish
+			err := t.handlePublish(topic, value)
 			if err != nil {
 				return err
 			}
@@ -144,11 +164,14 @@ func (t tuyaToMQTTService) Run(ctx context.Context) error {
 	return g.Wait()
 }
 
-func (t tuyaToMQTTService) handleMessageStatus(msg *Message) error {
+func (t *tuyaToMQTTService) handleMessageStatus(msg *Message) error {
 	for _, status := range msg.Status {
+		// build topic and value
 		topic := BuildMQTTTopicForStatus(t.params.MQTTTopic, msg, status)
 		value := strings.ReplaceAll(fmt.Sprintf("%#v", status.Value), "\"", "")
-		err := t.params.MQTTClient.Publish(topic, 2, true, []byte(value))
+
+		// publish
+		err := t.handlePublish(topic, value)
 		if err != nil {
 			return err
 		}
@@ -156,26 +179,56 @@ func (t tuyaToMQTTService) handleMessageStatus(msg *Message) error {
 	return nil
 }
 
-func (t tuyaToMQTTService) handleMessageDeviceManagement(msg *Message) error {
+func (t *tuyaToMQTTService) handleMessageDeviceManagement(msg *Message) error {
 	switch msg.BizCode {
 	case "nameUpdate":
 		if msg.BizData != nil && msg.BizData["name"] != nil {
 			if value, ok := msg.BizData["name"].(string); ok {
+				// build topic
 				topic := BuildMQTTTopicForStatusProperty(t.params.MQTTTopic, msg, "name")
-				err := t.params.MQTTClient.Publish(topic, 2, true, []byte(value))
+
+				// publish
+				err := t.handlePublish(topic, value)
 				if err != nil {
 					return err
 				}
 			}
 		}
 	case "online", "offline":
+		// build topic and value
 		topic := BuildMQTTTopicForStatusProperty(t.params.MQTTTopic, msg, "network")
-		err := t.params.MQTTClient.Publish(topic, 2, true, []byte(msg.BizCode))
+		value := msg.BizCode
+
+		// publish
+		err := t.handlePublish(topic, value)
 		if err != nil {
 			return err
 		}
 	default:
 		t.log.Warn().Fields(map[string]any{"msg": msg}).Msg("unknown message")
+	}
+	return nil
+}
+
+func (t *tuyaToMQTTService) handlePublish(topic, value string) error {
+	if value == "" {
+		return nil
+	}
+
+	// check if value is the same as last time
+	t.mqttCurrentStateMutex.RLock()
+	currentState, ok := t.mqttCurrentState[topic]
+	t.mqttCurrentStateMutex.RUnlock()
+
+	if ok && currentState == value {
+		return nil
+	}
+
+	// publish
+	t.log.Debug().Str("topic", topic).Str("value", value).Msg("publishing")
+	err := t.params.MQTTClient.Publish(topic, 2, true, []byte(value))
+	if err != nil {
+		return err
 	}
 	return nil
 }
